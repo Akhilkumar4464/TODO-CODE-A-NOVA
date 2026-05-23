91b2f92644ed0d5ded9758578c0ceb4f43a9c0e1
akhil kumar <niteshsharma4464@gmail.com>
2026-05-23 21:33:26 +0530

    Add initial frontend setup with React, Vite, and Tailwind CSS
    - Created main entry point (main.jsx) for React application.
    - Added Home, Login, and Signup components.
    - Included SVG assets for React and Vite logos.
    - Configured Vite with React and Tailwind CSS plugins.
    - Added global CSS file to import Tailwind CSS styles.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     {/* </Home>
+     </Login>
+     </Signup> */}
+
+    </>
+  )
+}
+
+export default App
